af83c690f8b4cd1c9bb830fc3bd3014c155752ba
Ramani-Shruti <shrutiramani001@gmail.com>
2026-09-02 18:52:06 +0530

    Time: 2 ms (63.37%), Space: 55.9 MB (37.02%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
@@ -1,29 +1,52 @@
-public class Solution {
-    public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] result = new int[nums1.Length+nums2.Length];
-        Array.Copy(nums1,0,result,0,nums1.Length);
-        Array.Copy(nums2,0,result,nums1.Length,nums2.Length);
-        Array.Sort(result);
-        int min;
-        double r;
-        min = result.Length/2;
-        if(result.Length%2 == 0)
+public class Solution
+{
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        int[] result = new int[nums1.Length + nums2.Length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < nums1.Length && j < nums2.Length)
         {
-            r = result[min -1] + result[min];
-            r = r/2;
-        }
-        else
-        {
-            if(min==result.Length)
+            if (nums1[i] <= nums2[j])
             {
-                r = result[0];
+                result[k] = nums1[i];
+                i++;
             }
             else
             {
-                r = result[min];
+                result[k] = nums2[j];
+                j++;
             }
-          
+
+            k++;
         }
-        return r;
+
+        // Remaining elements of nums1
+        while (i < nums1.Length)
+        {
+            result[k] = nums1[i];
+            i++;
+            k++;
+        }
+
+        // Remaining elements of nums2
+        while (j < nums2.Length)
+        {
+            result[k] = nums2[j];
+            j++;
+            k++;
+        }
+
+        int mid = result.Length / 2;
+
+        if (result.Length % 2 == 0)
+        {
+            return ((double)result[mid - 1] + result[mid]) / 2;
+        }
+
+        return result[mid];
     }
 }
